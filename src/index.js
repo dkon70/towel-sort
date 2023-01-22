3dfbbf675c712af541b084c143738dbbc3604398
Dmitry Konev <dkon70@inbox.ru>
2023-01-22 18:11:43 +0700

    fix: errors fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  if (!matrix) {
+  if (!matrix || matrix.length === 0) {
     return [];
   }
   res = [];
